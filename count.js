fa19aceb08f3ddd63bba563171781d916216564b
Richard Does Stuff <162537734+richardgrechko@users.noreply.github.com>
2026-05-29 16:07:25 +0500

    Create count.js

---
+++ count.js
@@ -0,0 +1,33 @@
+let count = 0n;
+window.writeTextAt = function writeTextAt(text,color,x,y) {
+   let k=text.split("\n")
+   void (function e(t) {
+      if(t>=k.length) {
+         return
+      }
+      void (function r(a) {
+         if(a>=k[t].length){
+            setTimeout(e,33,t+1)
+            return
+         }
+         (writeCharAt(k[t][a],color instanceof Array?color[(a-t)%color.length]:color,x+a,y+t),r(a+1),k[t][a])
+      })(0)
+   })(0)
+}
+w.on("msg",E=>{
+	let a = E.msg.toLowerCase().trim().split(" ")
+	if (a[0]==="!count") {
+		writeTextAt(" ".repeat(40),0,-20,-7);
+		writeTextAt(" ".repeat(40),0,-20,-6);
+		if (a[1]===(count+1n).toString()) {
+			count++;
+			writeTextAt("✓",[[64,255,64]],-20,-7);
+			writeTextAt(count.toString(),0,-20,-6);
+		} else {
+			count=0n;
+			writeTextAt("X",[[255,64,64]],-20,-7);
+			writeTextAt("Ruined the count!",[[255,128,128]],-20,-6);
+		}
+		writeTextAt(E.nick,E.color,-18,-7);
+	}
+})
